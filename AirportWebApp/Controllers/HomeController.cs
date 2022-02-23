7ed399706dd9d2dc981ac8b91cdd9de26ef79220
Ritesh Sinha <Ritesh.Sinha@mLogica.com>
2022-02-23 19:37:07 +0000

    I have added a time table to the site

--- AirportWebApp/Controllers/HomeController.cs
+++ AirportWebApp/Controllers/HomeController.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AirportWebApp;
 
 namespace AirportWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private AirportDB dB = new AirportDB();
         // GET: Home
         public ActionResult Home()
         {
             return View();
+        }
+        public ActionResult TimeTable(AirportTimeTable)
+        {
+            return View(dB.AirportTimeTables.ToList());
         }
     }
 }
